716e2901e6212c6659d234e5aa81546b6d4baa6d
ZackHu <hu.zixi@northeastern.edu>
2023-10-15 10:46:01 -0700

    add concrete player

---
+++ src/model/AIPlayer.java
@@ -0,0 +1,8 @@
+package model;
+
+public class AIPlayer extends Player{
+
+  public AIPlayer(String name, int currentRoom) {
+    super(name, currentRoom);
+  }
+}
---
+++ src/model/HumanPlayer.java
@@ -0,0 +1,8 @@
+package model;
+
+public class HumanPlayer extends Player {
+
+  public HumanPlayer(String name, int currentRoom) {
+    super(name, currentRoom);
+  }
+}
