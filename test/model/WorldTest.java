724d73db6d970646a8b6d0283e81c3c1f47bf471
ZackHu <hu.zixi@northeastern.edu>
2023-10-15 10:44:22 -0700

    rename package

---
+++ test/model/WorldTest.java
@@ -0,0 +1,53 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileReader;
+import java.io.IOException;
+import org.junit.Test;
+
+/**
+ * This class contains JUnit test cases for the {@link World} class, focusing on various aspects
+ * such as mansion parsing, neighbor relationships, and target information retrieval.
+ */
+public class WorldTest {
+
+  @Test
+  public void testGetMansion() throws IOException {
+    World world = new World();
+    world.setUp(new FileReader("res/mansion.txt"));
+    assertEquals(36, world.getMansion().getRow());
+    assertEquals(30, world.getMansion().getColumn());
+    assertEquals("Doctor Lucky's world.Mansion", world.getMansion().getName());
+  }
+
+  @Test
+  public void testParseString() throws IOException {
+    World world = new World();
+    world.setUp(new FileReader("res/mansion.txt"));
+    assertEquals(36, world.getMansion().getRow());
+    assertEquals(30, world.getMansion().getColumn());
+    assertEquals("Doctor Lucky's world.Mansion", world.getMansion().getName());
+    assertEquals(21, world.getMansion().getRoomList().size());
+    assertEquals(2, world.getMansion().getRoomList().get(8).getItemList().size());
+    assertEquals(8, world.getMansion().getRoomList().get(3).getNeightborList().size());
+  }
+
+  @Test
+  public void testIsNeighbor() throws IOException {
+    Room a = new Room("Dining Hall", new int[] { 12, 11, 21, 20 }, 1);
+    Room b = new Room("Armory", new int[] { 22, 19, 23, 26 }, 2);
+
+    assertTrue(World.isNeighbor(a, b));
+  }
+
+  @Test
+  public void testGetTarget() throws IOException {
+    World world = new World();
+    world.setUp(new FileReader("res/mansion.txt"));
+    assertEquals(50, world.getTarget().getHealth());
+    assertEquals("Doctor Lucky", world.getTarget().getName());
+    assertEquals(0, world.getTarget().getCurrentRoom());
+  }
+}
