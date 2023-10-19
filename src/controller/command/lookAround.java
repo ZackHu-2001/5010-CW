2b1ffb415381eb2c63f290d59592bf4f20de9d7f
ZackHu <hu.zixi@northeastern.edu>
2023-10-19 14:09:22 -0700

    update output format

--- src/controller/command/lookAround.java
+++ src/controller/command/lookAround.java
@@ -3,13 +3,23 @@ package controller.command;
 import controller.Command;
 import model.WorldModel;
 
+import java.io.IOException;
+
 public class lookAround implements Command {
-  public lookAround() {}
+  private final Appendable out;
+
+  public lookAround(Appendable out) {
+    this.out = out;
+  }
 
   public void act(WorldModel m) {
     if (m == null) {
       throw new IllegalArgumentException("model cannot be null");
     }
-    m.lookAround(m.getTurn());
+    try {
+      out.append(m.lookAround(m.getTurn()));
+    } catch (IOException ioe) {
+      throw new IllegalStateException("Append failed", ioe);
+    }
   }
 }
