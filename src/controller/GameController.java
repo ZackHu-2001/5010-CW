42e911e98e56ae5b9656030ec04f49f75969a6f0
ZackHu <hu.zixi@northeastern.edu>
2023-10-19 14:10:51 -0700

    update controller

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -18,6 +18,9 @@ public class GameController {
   private WorldModel worldModel;
   private final Scanner scan;
   private final Appendable out;
+  private int maxTurn;
+  private int currentTurn;
+  private final String reEnterPromot = "Please enter again: ";
 
   public GameController(Readable in, Appendable out) {
     if (in == null || out == null) {
@@ -30,7 +33,7 @@ public class GameController {
   /**
    * Add players, whether human player or computer player.
    */
-  public void addPlayer() {
+  private void addPlayer() {
     String next;
     try {
       // let the user decides whether create a human player or computer player
@@ -47,7 +50,8 @@ public class GameController {
         } else {
           out.append("Undetectable input: ")
               .append(next)
-              .append("\nPlease enter again: ");
+              .append("\n")
+              .append(reEnterPromot);
         }
       }
 
@@ -77,13 +81,15 @@ public class GameController {
                 .append(String.valueOf(position))
                 .append(", should be in range 1 to ")
                 .append(String.valueOf(roomNum))
-                .append(". \nPlease enter again: ");
+                .append(". \n")
+                .append(reEnterPromot);
           }
         } catch (NumberFormatException nfe) {
           out.append("Invalid position: ")
               .append(next)
               .append(", should be integer. ")
-              .append("\nPlease enter again: ");
+              .append("\n")
+              .append(reEnterPromot);
         }
 
       }
@@ -109,27 +115,58 @@ public class GameController {
   }
 
   /**
+   * Update current turn.
+   */
+  private void updateTurn() {
+    currentTurn += 1;
+  }
+
+  /**
    * Print current turn's player, and the items that the player holds.
    * Also displays the information of the room that the player current inside.
    */
-  public void printTurnInfo() {
+  private void printTurnInfo() {
     try {
+      out.append("\nTurn ")
+          .append(String.valueOf(currentTurn))
+          .append(", Doctor Lucky at room ")
+          .append(String.valueOf(worldModel.getTargePosition()));
+
       Player player = worldModel.getTurn();
       if (player.isHuman()) {
-        out.append("Information of current turn's human player: \n");
+        out.append("\nInformation of current turn's human player: \n");
       } else {
-        out.append("Information of current turn's computer player: \n");
+        out.append("\nInformation of current turn's computer player: \n");
       }
       out.append(player.toString())
           .append("In room ")
           .append(worldModel.getRoomInfo(player.getCurrentRoom()));
 
-      out.append("\nAvailable options in this turn:\n")
+      out.append("Available options of this turn:\n")
           .append("1. look around (show neighbor room information)\n")
-          .append("2. move (move to a neighbor room)\n")
-          .append("3. pick item (pick up item in the room)\n");
+          .append("2. move (move to a neighbor room)\n");
+
+      if (!worldModel.showItems(player).equals("[Empty]")) {
+        out.append("3. pick item (pick up item in the room)\n");
+      }
+
     } catch (IOException ioe) {
       throw new IllegalStateException("Append failed", ioe);
+    }
+  }
+
+  /**
+   * The purpose of this method is to make sure that, when player have no item
+   * then the command pick item should be temporarily removed from the options.
+   *
+   * @param worldModel Would model to tell if player have item.
+   * @param knownCommands Known command map to be modified.
+   */
+  private void upDateCommands(WorldModel worldModel, Map<String, Function<Scanner, Command>> knownCommands) {
+    if (worldModel.showItems(worldModel.getTurn()).equals("[Empty]")) {
+      knownCommands.remove("pick item");
+    } else {
+      knownCommands.put("pick item", s -> new pickItem(scan, out));
     }
   }
 
@@ -141,19 +178,15 @@ public class GameController {
    * @param worldModel The model that it controls over.
    */
   public void startGame(WorldModel worldModel) {
-
+    // validate world model
     if (worldModel == null) {
       throw new IllegalArgumentException("Invalid model.");
     }
-
     this.worldModel = worldModel;
+
+    // add player
     try {
-      out.append("Start game by adding a playe\n" +
-          "\n" +
-          "    Map<String, Function<Scanner, Command>> knownCommands = new HashMap<>();\n" +
-          "    knownCommands.put(\"move\", s -> new movePlayer(s.nextInt()));\n" +
-          "    knownCommands.put(\"look around\", s -> new lookAround());\n" +
-          "    knownCommands.put(\"pick item\", s -> new pickItem());\nr? Enter y to add player and start, anything else to quit.\n");
+      out.append("Start game by adding a player? Enter y to add player and start, anything else to quit.\n");
       String input = scan.next();
       if (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes")) {
         out.append("\nLet's add players.\n");
@@ -176,29 +209,40 @@ public class GameController {
         }
       }
 
+      // initialize all commands
+      Map<String, Function<Scanner, Command>> knownCommands = new HashMap<>();
+      knownCommands.put("look around", s -> new lookAround(out));
+      knownCommands.put("move", s -> new movePlayer(s.nextInt()));
+
       out.append("All players loaded, game starts now!\n");
       printTurnInfo();
+      updateTurn();
+      upDateCommands(worldModel, knownCommands);
 
+      // handle user input command
       while (scan.hasNext()) {
         Command command;
-        String in = scan.next();
+        String in = scan.nextLine();
         if (in.equalsIgnoreCase("q") || in.equalsIgnoreCase("quit")) {
           return;
         }
         Function<Scanner, Command> cmd = knownCommands.getOrDefault(in, null);
         if (cmd == null) {
-          throw new IllegalArgumentException();
+          out.append("Invalid input. Commands like [look around], [move], [pick item] expected.\n");
+          out.append(reEnterPromot);
+          scan.nextLine();
+          continue;
         } else {
           command = cmd.apply(scan);
           command.act(worldModel);
         }
-        printTurnInfo();
 
+        printTurnInfo();
+        updateTurn();
+        upDateCommands(worldModel, knownCommands);
       }
     } catch (IOException ioe) {
       throw new IllegalStateException("Append failed", ioe);
     }
-
   }
-
 }
