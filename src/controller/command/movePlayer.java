8713364b2f4eedce0d29c09b494e42686d754162
ZackHu <144851646+ZackHu-2001@users.noreply.github.com>
2023-10-19 16:57:40 -0700

    update move player's logic

--- src/controller/command/movePlayer.java
+++ src/controller/command/movePlayer.java
@@ -1,12 +1,17 @@
 package controller.command;
 
 import controller.Command;
+import java.io.IOException;
+import java.util.Scanner;
 import model.WorldModel;
 
 public class movePlayer implements Command {
-  int targetRoom;
-  public movePlayer(int targetRoom) {
-    this.targetRoom = targetRoom;
+  private final Scanner scan;
+  private final Appendable out;
+
+  public movePlayer(Scanner scan, Appendable out) {
+    this.scan = scan;
+    this.out = out;
   }
 
   @Override
@@ -14,7 +19,30 @@ public class movePlayer implements Command {
     if (m == null) {
       throw new IllegalArgumentException("model cannot be null");
     }
-    m.movePlayer(m.getTurn(), targetRoom);
+
+    int targetRoomId;
+    try {
+      out.append("Please enter the index of the room you want to move to: ");
+      String next;
+
+      while (scan.hasNextLine()) {
+          next = scan.nextLine();
+          try {
+            targetRoomId = Integer.parseInt(next);
+            if (!m.movePlayer(m.getTurn(), targetRoomId - 1)) {
+              out.append("\nCan not move to room ")
+                  .append(String.valueOf(targetRoomId))
+                  .append(", not neighbor of current room.\nPlease enter again: ");
+            }
+
+          } catch (NumberFormatException nfe) {
+            out.append("Invalid room id, integer expected.\nPlease enter again: ");
+          }
+      }
+    } catch (IOException ioe) {
+      throw new IllegalStateException("Append failed", ioe);
+    }
+//    m.movePlayer(m.getTurn(), targetRoom);
   }
 
 }
