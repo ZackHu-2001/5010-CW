cf699123f7579a2a130ddc9ea8159e61c89c53bd
ZackHu <hu.zixi@northeastern.edu>
2023-10-22 11:20:40 -0700

    clean up

--- src/Driver.java
+++ src/Driver.java
@@ -1,16 +1,15 @@
-import controller.GameController;
-import model.Room;
-import model.World;
-import model.WorldModel;
 
 import static java.lang.System.exit;
-import static java.lang.System.out;
 
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.util.List;
+import controller.GameController;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.Scanner;
-import javax.imageio.ImageIO;
+import model.World;
+import model.WorldModel;
 
 
 /**
@@ -26,7 +25,6 @@ public class Driver {
    */
   public static void main(String[] args) {
     StringReader filePath = new StringReader("res/mansion.txt\n");
-    StringReader command = new StringReader("jkl\n10\ny\nh\nzack\n2\nn\nlook around\nmove\n2\n1\n1\n0");
     Appendable output = System.out;
 
     // read user input: the mansion file, or direct string input
@@ -36,7 +34,6 @@ public class Driver {
     if (args.length != 0) {
       pathToFile = args[0];
     } else {
-      System.out.println("Please provide the path to the world file:");
       pathToFile = new Scanner(filePath).nextLine();
     }
 
@@ -50,7 +47,6 @@ public class Driver {
       output.append(worldModel.toString());
 
       GameController gameController = new GameController(new InputStreamReader(System.in), output);
-//      GameController gameController = new GameController(command, output);
       gameController.startGame(worldModel);
 
     } catch (IOException e) {
