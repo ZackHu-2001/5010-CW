e4fee05dc35c84f4c7983751c5cfe639a599cb2e
ZackHu <hu.zixi@northeastern.edu>
2023-11-04 17:33:48 -0700

    add command attack

---
+++ src/controller/command/Attack.java
@@ -0,0 +1,12 @@
+package controller.command;
+
+import controller.Command;
+import model.WorldModel;
+
+public class Attack implements Command {
+
+  @Override
+  public void act(WorldModel m) {
+
+  }
+}
