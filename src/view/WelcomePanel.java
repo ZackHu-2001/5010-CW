3280d8f6f40cb475a4496aabc1a7a16768073527
ZackHu <144851646+ZackHu-2001@users.noreply.github.com>
2023-12-01 20:48:33 -0800

    add welcome panel

---
+++ src/view/WelcomePanel.java
@@ -0,0 +1,41 @@
+package view;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class WelcomePanel extends JPanel {
+  private JLabel gameNameLabel;
+  private JLabel authorLabel;
+  public static int WIDTH = 600;
+  public static int HEIGHT = 350;
+
+
+  public WelcomePanel() {
+    setLayout(new GridBagLayout());
+    setSize(600, 400);
+
+    // Create GridBagConstraints
+    GridBagConstraints gbc = new GridBagConstraints();
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.weightx = 1.0;
+    gbc.weighty = 1.0;
+    gbc.fill = GridBagConstraints.BOTH;
+
+    gameNameLabel = new JLabel("Kill Doctor Lucky");
+    gameNameLabel.setFont(new Font("Arial", Font.ITALIC, 42));
+    gameNameLabel.setHorizontalAlignment(JLabel.CENTER);
+    gameNameLabel.setVerticalAlignment(JLabel.CENTER);
+    gbc.insets = new Insets(100,0,0, 0);
+    add(gameNameLabel, gbc);
+
+    gbc.gridy = 1;
+
+    authorLabel = new JLabel("Zack Hu");
+    authorLabel.setFont(new Font("Arial", Font.PLAIN, 22));
+    authorLabel.setHorizontalAlignment(JLabel.RIGHT);
+    authorLabel.setVerticalAlignment(JLabel.CENTER);
+    gbc.insets = new Insets(0,0,100, 70);
+    add(authorLabel, gbc);
+  }
+}
