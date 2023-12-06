d901a5354814fff31880d77f767b0a105b092e92
ZackHu <144851646+ZackHu-2001@users.noreply.github.com>
2023-12-06 12:54:59 -0800

    refactor: apply mvc pattern to driver
    the initial version does not follow the MVC pattern

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,4 @@
+import controller.Controller;
 import controller.GameController;
 import java.io.FileReader;
 import java.io.IOException;
@@ -6,6 +7,8 @@ import java.io.Reader;
 import java.util.Scanner;
 import model.World;
 import model.WorldModel;
+import view.View;
+import view.ViewFactory;
 
 /**
  * This driver class helps manipulate the World class
@@ -15,67 +18,21 @@ public class Driver {
   /**
    * In main, we can manipulate the World in an interactive way.
    *
-   * @param args          File path could be passed through
-   *                      command line arguments.
+   * @param args          Game's playing mode is given through args.
+   *                      For example, the CMD mode or the GUI mode.
    */
   public static void main(String[] args) {
-    Readable input = new InputStreamReader(System.in);
-    Appendable output = System.out;
 
-    // read user input: the mansion file, or direct string input
-    Reader fileReader;
-    String pathToFile;
+    // initialize world model
+    WorldModel model = new World();
 
-    System.out.println("Please input path to the file: ");
+    // initialize view factory to create view
+    ViewFactory viewFactory = new ViewFactory();
+    View view = viewFactory.createView(args[0], model);
 
-    if (args.length != 0) {
-      pathToFile = args[0];
-    } else {
-      pathToFile = new Scanner(System.in).nextLine();
-    }
+    // initialize controller
+    Controller controller = new GameController(model, view);
 
-    try {
-      fileReader = new FileReader(pathToFile);
-      WorldModel worldModel = new World(fileReader);
-
-      System.out.println("Successfully read in the file!\n\n"
-          + "Here are detailed information:");
-
-      output.append(worldModel.toString());
-
-      // read in max turn
-      int maxTurn = 0;
-      String inputString;
-      Scanner scan = new Scanner(System.in);
-
-      output.append("What is the max turn of the game?\n");
-      while (scan.hasNextLine()) {
-        inputString = scan.nextLine();
-        if ("quit".equalsIgnoreCase(inputString)
-            || "q".equalsIgnoreCase(inputString)) {
-          output.append("Exit game, have a nice day~\n");
-          return;
-        }
-        try {
-          maxTurn = Integer.valueOf(inputString);
-          if (maxTurn <= 0) {
-            output.append("Invalid max turn, one positive integer expected.\n");
-            continue;
-          }
-          break;
-        } catch (NumberFormatException nfe) {
-          output.append("Invalid max turn, one positive integer expected.\n")
-              .append("Please enter again:");
-        }
-      }
-
-      GameController gameController = new GameController(input, output, maxTurn);
-      gameController.startGame(worldModel);
-
-    } catch (IOException e) {
-      System.out.println("There are problems with path to file, exit now.");
-      System.exit(1);
-    }
-
+    controller.playGame();
   }
 }
